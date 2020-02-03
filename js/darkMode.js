7d8492989595398be660733a2bcec3c39a5aaa45
unknown <v25a07@gmail.com>
2020-02-02 20:45:11 -0500

    Se finit

---
+++ js/darkMode.js
@@ -0,0 +1,21 @@
+function darkMode() {
+  if (document.body.style.backgroundColor == "black") {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    if (location.pathname === "/en.html") {
+      document.getElementById("cambioTema").innerHTML = "Dark mode";
+    } else {
+      document.getElementById("cambioTema").innerHTML = "Modo noche";
+    }
+  } else {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    if (location.pathname === "/en.html") {
+      document.getElementById("cambioTema").innerHTML = "Light mode";
+    } else {
+      document.getElementById("cambioTema").innerHTML = "Modo día";
+    }
+  }
+}
+
+this.document.getElementById("cambioTema").addEventListener("click", darkMode);
--- js/main.js
+++ js/main.js
@@ -1,32 +1,19 @@
 /**
  * Método que redirije a la página en inglés
  */
-$("button.botonInicio.eng").click(function() { 
-    setTimeout(function() {
+function btnIngles() {
+  setTimeout(function() {
     location.href = "/en.html";
-  }, 1000);
-});
+  }, 500);
+}
 /**
  * Método que redirije a la página en español
  */
-$("button.botonInicio.es").click(function() {
+function btnEspanol() {
   setTimeout(function() {
     location.href = "/es.html";
   }, 500);
-});
+}
 
-/**
- * Método que cambia el tema a modo noche
- */
-
-$("#cambioTema").click(function() {
-  if (this.document.body.style.backgroundColor == "black") {
-    this.document.body.style.backgroundColor = "white";
-    this.document.body.style.color = "black";
-    this.document.getElementById("cambioTema").innerHTML = "Modo noche";
-  } else {
-    this.document.body.style.backgroundColor = "black";
-    this.document.body.style.color = "white";
-    this.document.getElementById("cambioTema").innerHTML = "Modo día";
-  }
-});
+this.document.getElementById("btnIngles").addEventListener("click", btnIngles);
+this.document.getElementById("btnEspanol").addEventListener("click", btnEspanol);
